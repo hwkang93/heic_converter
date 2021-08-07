0eaee04f620d23d4cfaed0a8673169c71c0d0cab
hwkang <landman93@naver.com>
2021-08-07 22:38:12 +0900

    HEIC TO JPEG 모듈 완성 (주석 추가하고 끝 !)

--- converter.js
+++ converter.js
@@ -5,6 +5,7 @@ const piexif = require('piexifjs')
 const exifr = require('exifr');
 const yaml = require('js-yaml');
 
+//yaml 파일 읽기
 const readOptions = () => {
     try {
         const file = fs.readFileSync('./options.yaml', 'utf-8');
@@ -18,6 +19,7 @@ const readOptions = () => {
     }
 }
 
+//확장자 heic인지 확인
 const isHeic = file => {
     const dotLoc = file.lastIndexOf('.');
     const extension = file.substring(dotLoc, file.length).toLowerCase();
@@ -32,7 +34,8 @@ const outputExtension = 'JPEG';
 (
     async () => {
         console.log('******** FILE CONVERTER START ********')
-
+        
+        //1. heic 확장자 파일 리스트 조회
         const files = await util.promisify(fs.readdir)(folderPath)
             .then(files => files.filter(isHeic))
             .catch(error => console.error(error));
@@ -49,10 +52,12 @@ const outputExtension = 'JPEG';
             console.log(`   >> output file path : ${outputPath}`)
             
             try {
+                //2. Metadata > Date 읽기
                 const date = await exifr.parse(filePath)
                     .then(output => new Date(output.DateTimeOriginal))
                     .catch(err => undefined);
     
+                //3. Metadata > Geometry 읽기
                 const location = await exifr.gps(filePath);
                 const inputBuffer = await util.promisify(fs.readFile)(filePath);
                 const outputBuffer = await convert({
@@ -60,15 +65,20 @@ const outputExtension = 'JPEG';
                     format: outputExtension,
                     quality: 1
                 })
-    
+                
+                //4. HEIC >  JPEG로 변환 ( 결과 JPEG 파일에는 메타데이터 없음 )
                 fs.writeFileSync(outputPath, outputBuffer);
                 
+                //5. JPEG 파일 읽기
                 const data = fs.readFileSync(outputPath).toString('binary');
                 const exifObj = piexif.load(data);
+
+                //6. JPEG 파일에 DATE 메타데이터 추가
                 if(date) {
                     exifObj.Exif[piexif.ExifIFD.DateTimeDigitized] = dateToString(date);
                 }
     
+                //7. JPEG 파일에 Geometry 메타데이터 추가
                 if(location) {
                     exifObj.GPS[piexif.GPSIFD.GPSLatitude] =  degToDmsRational(location.latitude);
                     exifObj.GPS[piexif.GPSIFD.GPSLatitudeRef] =  "N";
@@ -76,6 +86,7 @@ const outputExtension = 'JPEG';
                     exifObj.GPS[piexif.GPSIFD.GPSLongitudeRef] =  "W";
                 }
                 
+                //8. 결과 JPEG 생성 (기존 JPEG 파일과 같은 이름으로 만듦)
                 const exifByte = piexif.dump(exifObj);
                 const newData = piexif.insert(exifByte, data);
                 const newJpeg = Buffer.from(newData, "binary");
